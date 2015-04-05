716904f1d255c177dc280888e454ed3b880eca3c
mp-jet <no-9901@yandex.ru>
2015-04-05 14:43:16 +0300

    Finish static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+SampleApp::Application.config.secret_key_base = 'df5d3a8b032e65c75f85d22a92329fb93a8a6df0c5a596d57542de425741537997335e36f9ee12a5d2d6e8ac55a4de970ac452bf320cb7a48bdba5c0dd5ef720'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
+#Sample_App::Application.config.secret_key_base = 'bla' df5d3a8b032e65c75f85d22a92329fb93a8a6df0c5a596d57542de425741537997335e36f9ee12a5d2d6e8ac55a4de970ac452bf320cb7a48bdba5c0dd5ef720
 Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
